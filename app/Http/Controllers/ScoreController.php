5dc0a9ecd9a48b13178b1033d2b98e05c9f4f2bc
bagusyanuar <50134851+bagusyanuar@users.noreply.github.com>
2021-09-01 15:36:55 +0700

    Update ScoreController.php

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -60,23 +60,61 @@ class ScoreController extends CustomController
             }])->get();
             $arrData = $data->toArray();
             $result = [];
+            $chkSum = 0;
+            $scoreMin = 1;
+            $scoreMax = 3;
             foreach ($arrData as $v) {
                 $index = $v['name'];
+                $weight = $v['weight'];
                 $value = 0;
-                foreach ($v['sub_indicator'] as $sub)
-                {
+                $subLength = count($v['sub_indicator']);
+                $chkSum += $v['weight'];
+                $min = ($scoreMin * $subLength);
+                $max = ($scoreMax * $subLength);
+                $maxFactor = round((100 * $weight), 2, PHP_ROUND_HALF_UP);
+                $radarMin = 0;
+                $radarMax = 10;
+                $a = round(($radarMax - $radarMin) / ($max - $min), 3, PHP_ROUND_HALF_UP);
+                $b = round($radarMax - ($max * $a), 0, PHP_ROUND_HALF_UP);
+                foreach ($v['sub_indicator'] as $sub) {
                     $tmpScore = $sub['single_score'] !== null ? $sub['single_score']['score'] : 0;
                     $value += $tmpScore;
                 }
+
+
+                $checkConversion = ($a * $max) + $b;
+                $a_cumulative = round(($maxFactor / ($max - $min)), 3, PHP_ROUND_HALF_UP);
+                $b_cumulative = round(($maxFactor - ($max * $a_cumulative)), 3, PHP_ROUND_HALF_UP);
+                $radar = 0;
+                $cumulative = 0;
+                if ($value > 0) {
+                    $radar = ($a * $value) + $b;
+                    $cumulative = ($a_cumulative * $value) + $b_cumulative;
+                }
                 $transform = [
                     'index' => $index,
-                    'value' => $value
+                    'weight' => $weight,
+                    'sub_length' => $subLength,
+                    'min' => $min,
+                    'max' => $max,
+                    'max_factor' => $maxFactor,
+                    'check_conversion' => round($checkConversion, 0, PHP_ROUND_HALF_UP),
+                    'a' => $a,
+                    'b' => $b,
+                    'a_cumulative' => $a_cumulative,
+                    'b_cumulative' => $b_cumulative,
+                    'value' => $value,
+                    'radar' => $radar,
+                    'cumulative' => $cumulative,
                 ];
                 array_push($result, $transform);
             }
             return response()->json([
                 'msg' => 'success',
-                'data' => $result
+                'data' => [
+                    'indicator' => $result,
+                    'chk_summary' => round($chkSum, 0, PHP_ROUND_HALF_UP)
+                ],
             ], 200);
         } catch (\Exception $e) {
             return response()->json(['msg' => 'Terjadi Kesalahan Server..' . $e], 500);
