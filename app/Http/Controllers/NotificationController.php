ab5d18e7ac303eb85d54ae2bd8d7d0d8a1b992de
portgazdbeel <portgaz77@gmail.com>
2021-09-22 17:29:20 +0700

    commit

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notification;
+use Illuminate\Http\Request;
+
+class NotificationController extends Controller
+{
+    //
+
+    public function notif(){
+        $notif = Notification::all();
+        return $notif;
+    }
+}
