f5635e1f7b4ca924215f4cf84b9a3a1588b15ed5
pradana <pradanamahendra@gmail.com>
2021-09-28 13:10:42 +0700

    asd

--- resources/views/superuser/dashboard/accessor.blade.php
+++ resources/views/superuser/dashboard/accessor.blade.php
@@ -3,7 +3,7 @@
         <p class="fw-bold t-primary">Dashboard</p>
         <div role="tablist" id="tablist">
             <div class="items-tab" id="menu-tab">
-                <a class="card-tab  d-block c-text card-user" id="user">
+                <a class="card-tab  d-block c-text card-user" style="background-color: rgba(255, 224, 224, 20)" id="user">
                     <div class="d-flex justify-content-between">
                         <i class='bx bx-user-circle icon-size-lg '></i>
                         <p class="number-card">0</p>
@@ -13,7 +13,7 @@
                     </div>
                 </a>
 
-                <a class="card-tab d-block c-text card-user" id="package">
+                <a class="card-tab d-block c-text card-user" style="background-color: rgba(224, 255, 224, 20)" id="package">
                     <div class="d-flex justify-content-between">
                         <i class='bx bx-building-house icon-size-lg'></i>
                         <p class="number-card">0</p>
@@ -23,9 +23,9 @@
                     </div>
                 </a>
 
-                <a class="card-tab d-block c-text card-user" id="indicator">
+                <a class="card-tab d-block c-text card-user" style="background-color: rgba(224, 224, 255, 20)" id="indicator">
                     <div class="d-flex justify-content-between">
-                        <i class='bx bx-doughnut-chart icon-size-lg'></i>
+                        <i class='bx bx-receipt icon-size-lg'></i>
                         <p class="number-card">0</p>
                     </div>
                     <div class="mt-2">
@@ -37,21 +37,21 @@
             </div>
         </div>
     </div>
-    <div style="padding-right: 30px; padding-left: 30px">
+    <div style="padding-right: 30px; padding-left: 30px" class="mt-5">
         <div class="d-flex justify-content-between">
 
             <p class="fw-bold t-primary">Data Penyedia Jasa</p>
-{{--            <div class="search">--}}
-{{--                <input type="text" placeholder="search"/>--}}
-{{--                <div class="symbol">--}}
-{{--                    <svg style="width:25px;height:25px" class="cloud" viewBox="0 0 25 25">--}}
-{{--                        <path fill="white"--}}
-{{--                              d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"/>--}}
-{{--                    </svg>--}}
-{{--                </div>--}}
-{{--            </div>--}}
+            {{-- <div class="search"> --}}
+            {{-- <input type="text" placeholder="search"/> --}}
+            {{-- <div class="symbol"> --}}
+            {{-- <svg style="width:25px;height:25px" class="cloud" viewBox="0 0 25 25"> --}}
+            {{-- <path fill="white" --}}
+            {{-- d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"/> --}}
+            {{-- </svg> --}}
+            {{-- </div> --}}
+            {{-- </div> --}}
         </div>
-        <div class="row g-3 row-cols-xl-4 row-cols-lg-3 row-cols-md-2">
+        {{-- <div class="row g-3 row-cols-xl-4 row-cols-lg-3 row-cols-md-2">
             <div class="col">
                 <div class="card-vendor-2 d-flex">
                     <div class="left-box-card" style="width: 80px; margin-right: 10px">
@@ -72,13 +72,13 @@
             <div class="col">
                 <div class="card-vendor-2"></div>
             </div>
+        </div> --}}
+
+        <div role="tablist" class="row" id="tablist">
+            <div class="items-tab col-3" id="menu-vendor">
+
+            </div>
         </div>
-
-{{--        <div role="tablist" class="row" id="tablist">--}}
-{{--            <div class="items-tab col-3"  id="menu-vendor">--}}
-
-{{--            </div>--}}
-{{--        </div>--}}
     </div>
 
 
@@ -90,10 +90,11 @@
         });
 
         function elVendor(data) {
-            return '<a href="/penilaian?vendor=' + data['id'] +'" class="card-vendor-2 d-block c-text card-user">' +
+            return '<a href="/penilaian?vendor=' + data['id'] + '" class="card-vendor-2 d-block c-text card-user">' +
                 'abcd' +
                 '</a>';
         }
+
         function getVendor() {
             var vendor = $('#menu-vendor');
             vendor.empty();
@@ -105,9 +106,13 @@
                     vendor.append(' <a href="/penilaian?vendor=' + value['id'] +
                         '" class="card-vendor d-block c-text card-user" id="">\n' +
                         '                    <div class="d-flex justify-content-left">\n' +
-                        '                        <div class="div-image"> <img src="'+value['image']+'" onerror="this.onerror=null; this.src=\'{{ asset('/images/noimage.png') }}\'"/> </div>\n' +
-                        ' <div class="w-100"><p  class="nama">'+ value['vendor']['name'] +'</p><p class="email">'+value['email']+'</p> <h6 class="t-right number"> '+value['package_vendor_going'].length+' </h6> </div>' +
-                        '                    </div>\n' +
+                        '                        <div class="div-image"> <img src="' + value['image'] +
+                        '" onerror="this.onerror=null; this.src=\'{{ asset('/images/noimage.png') }}\'"/> </div>\n' +
+                        '<div class="d-flex flex-column w-100  justify-content-between" style="height: 130px"> ' +
+                        ' <div class="w-100"><p  class="nama">' + value['vendor']['name'] +
+                        '</p><p class="email">' + value['email'] + '</p>  </div>' +
+                        '                    <h6 class="t-right number"> ' +
+                        value['package_vendor_going'].length + ' </h6></div></div>\n' +
 
                         '                </a>')
                 })
