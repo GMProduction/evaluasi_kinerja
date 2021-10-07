5c211bff21807f0de1a73ea1630add982f74bc33
pradana <pradanamahendra@gmail.com>
2021-10-07 09:30:44 +0700

    asd

--- resources/views/superuser/indikator/indikator.blade.php
+++ resources/views/superuser/indikator/indikator.blade.php
@@ -41,7 +41,7 @@
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel"><span id="title"></span> Data Indikator</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
                         <form id="form" onsubmit="return Save()">
--- resources/views/superuser/paket-konstruksi/paketKonstruksi.blade.php
+++ resources/views/superuser/paket-konstruksi/paketKonstruksi.blade.php
@@ -36,7 +36,7 @@
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">Tambah Data Konstruksi</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
                         <form id="form" onsubmit="return Save()">
