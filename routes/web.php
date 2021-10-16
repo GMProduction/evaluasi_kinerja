60bf979ebcabce97792feda49b70c7749beeae51
portgazdbeel <portgaz77@gmail.com>
2021-10-16 15:19:29 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', [\App\Http\Controllers\AuthController::class, 'pageLogin'])->name('login');
+//Route::get('/login', [\App\Http\Controllers\AuthController::class, 'pageLogin'])->name('login');
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
 Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
 
@@ -28,7 +28,7 @@ Route::get(
     function () {
         return view('login');
     }
-);
+)->middleware('guest');
 
 Route::prefix('/')->middleware('auth')->group(
     function () {
